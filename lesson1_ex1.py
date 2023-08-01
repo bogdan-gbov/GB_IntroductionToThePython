0ddd3b67909f256730355b80a528afbcd08ce58b
Bogdan Gololobov <arvedan90@gmail.com>
2023-08-01 21:40:54 +0300

    добавил решение для задачи

--- lesson1_ex1.py
+++ lesson1_ex1.py
@@ -3,3 +3,12 @@
 
 Результат сохраните в перменную res.
 """
+
+num = input('Введите трехзначное число: ')
+result = 0
+if len(num) == 3:
+    for i in num:
+        result = result + int(i)
+    print(result)
+else:
+    print('Вы ввели не трехзначное число')
